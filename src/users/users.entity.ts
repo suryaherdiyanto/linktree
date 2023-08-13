4acafe1621e713595bf4e80205614809242adb64
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-13 21:10:12 +0800

    add: user.entity.ts

---
+++ src/users/users.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    @Index()
+    id: string;
+
+    @Column('string')
+    @Index({ unique: true })
+    username: string;
+
+    @Column('string')
+    @Index({ unique: true })
+    email: string;
+
+    @Column('string')
+    password: string;
+}
