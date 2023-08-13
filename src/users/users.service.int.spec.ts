f001f948a607bfc4262718e37084b3ffbf639372
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-13 22:03:20 +0800

    update: integration test command and setup int test for users.service

--- src/users/users.service.int.spec.ts
+++ src/users/users.service.int.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersModule } from './users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseOption } from '../config/database.config';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersModule],
+      imports: [
+        TypeOrmModule.forRoot(databaseOption)
+      ]
     }).compile();
 
     service = module.get<UsersService>(UsersService);
