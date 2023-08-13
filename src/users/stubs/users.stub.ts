e83a5838732aa895916e6bc64e74bd23c3787b32
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-13 21:20:53 +0800

    add: users stub

---
+++ src/users/stubs/users.stub.ts
@@ -0,0 +1,15 @@
+
+export const users = [
+    {
+        username: 'dragon123',
+        email: 'dragon@gmail.com',
+        password: 'secret',
+        name: 'Monkey D. Dragon'
+    },
+    {
+        username: 'hang',
+        email: 'hang@icloud.com',
+        password: 'secret',
+        name: 'John Doe'
+    }
+]
