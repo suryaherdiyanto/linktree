9197fda3821b3ab3001a7457b1f2c443a52f350f
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-19 10:46:41 +0800

    add: register logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Post } from '@nestjs/common';
+import {CreateUserDTO} from './dtos/create-user.dto';
+import {UsersService} from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+	constructor(private userService: UsersService) {}
+
+	@Post('/register')
+	async register(data: CreateUserDTO) {
+		const isEmailExists = await this.userService.findByEmail(data.email);
+
+		if (isEmailExists) {
+			throw new BadRequestException(`Email: ${data.email} already been used!`);
+		}
+
+		const isUsernameExists = await this.userService.findByUsername(data.username);
+
+		if (isUsernameExists) {
+			throw new BadRequestException(`Username: ${data.username} already been used!`);
+		}
+
+		const user = this.userService.create(data)
+
+		return { message: "Successfully registered!", data: user }
+	}
+}
