14d9c6dd4c0e274d460def2c8cacb4d22720face
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-26 12:06:46 +0800

    setup repository for testing

--- src/profiles/profiles.service.int.spec.ts
+++ src/profiles/profiles.service.int.spec.ts
@@ -1,8 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesService } from './profiles.service';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/users.entity';
+import { Profile } from './profiles.entity';
+import { users } from 'src/users/stubs/users.stub';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ProfilesService', () => {
   let service: ProfilesService;
+  let userRepository: Repository<User>
+  let profileRepository: Repository<Profile>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,6 +17,10 @@ describe('ProfilesService', () => {
     }).compile();
 
     service = module.get<ProfilesService>(ProfilesService);
+    userRepository = module.get(getRepositoryToken(User));
+    profileRepository = module.get(getRepositoryToken(Profile));
+
+    await userRepository.createQueryBuilder().insert().values(users).execute();
   });
 
   it('should be defined', () => {
