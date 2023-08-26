0fb9177bd6458f2c2ecd6bc9832b664258f33fa1
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-26 20:07:19 +0800

    add jwt guard

---
+++ src/users/guards/jwt.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Request } from "express";
+import { Observable } from "rxjs";
+import * as Jwt from 'jsonwebtoken';
+
+@Injectable()
+export class JWTGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest<Request>();
+        const bearerToken = request.headers.authorization;
+
+        if (!bearerToken) {
+            throw new UnauthorizedException("Unauthenticated");
+        }
+
+        const [_, token] = bearerToken.split('Bearer ');
+        const decodedUser = Jwt.verify(token, 'verfysecretkey');
+
+        if (!decodedUser) {
+            throw new UnauthorizedException();
+        }
+
+        request['user'] = decodedUser;
+
+        return true;
+    }
+}
