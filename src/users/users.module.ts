6e127b0630741d0495a47b6464d285f7b61eb82a
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-26 16:38:10 +0800

    include the profile entity in the users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { Profile } from '../profiles/profiles.entity';
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
-  imports: [TypeOrmModule.forFeature([User])]
+  imports: [TypeOrmModule.forFeature([User, Profile])]
 })
 export class UsersModule {}
