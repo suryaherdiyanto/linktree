4f044e5a56819b15a0aee7ae1a7f690be9b90b3a
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-27 16:03:11 +0800

    configure the storage module

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { StorageService } from './storage.service';
 
-@Module({})
+@Module({
+    providers: [StorageService],
+    exports: [StorageService]
+})
 export class StorageModule {}
