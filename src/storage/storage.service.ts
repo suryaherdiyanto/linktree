a976a1b9c165ee2a84fbca5f9392434af8e8e1ad
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-27 16:02:03 +0800

    integrate storage service with s3

---
+++ src/storage/storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { S3Storage } from "./s3.storage";
+import { CanStoreResource } from "./interfaces/storage.interface";
+import { ConfigService } from "@nestjs/config";
+
+
+@Injectable()
+export class StorageService {
+    private storage: CanStoreResource;
+
+    constructor(private configService: ConfigService) {
+        this.storage = new S3Storage({
+                region: this.configService.get<string>('AWS_REGION', ''),
+                bucket: this.configService.get<string>('AWS_BUCKET', ''),
+                accessKey: this.configService.get<string>('AWS_ACCESKEY', '')
+            }
+        );
+    }
+
+    uploadFile(data: Buffer, filename: string) {
+        return this.storage.put(data, filename);
+    }
+}
