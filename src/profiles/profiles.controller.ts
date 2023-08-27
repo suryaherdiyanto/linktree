4a3818389c81c13db73c76715eeb899b6f4a584f
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-27 17:21:46 +0800

    update the file upload flow in save profile api

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Put, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Put, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { JWTGuard } from '../users/guards/jwt.guard';
 import { ProfilesService } from './profiles.service';
 import { User as UserJWT } from '../users/decorators/jwt-user.decorator';
@@ -6,6 +6,7 @@ import { User } from '../users/users.entity';
 import { UpdateProfileDTO } from './dtos/update-profile.dto';
 import { StorageService } from 'src/storage/storage.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as crypto from 'crypto';
 
 @Controller('profiles')
 export class ProfilesController {
@@ -15,9 +16,16 @@ export class ProfilesController {
     @Put('/update')
     @UseGuards(JWTGuard)
     @UseInterceptors(FileInterceptor('photo'))
-    async updateProfile(@UserJWT() user: Partial<User>, @UploadedFile() file: Express.Multer.File, @Body() data: UpdateProfileDTO) {
-        const upload = await this.storageService.uploadFile(file.buffer, 'test');
-        console.log(upload);
-        return this.profileService.saveProfile(user.id, data.bio, data.birthday);
+    async updateProfile(@UserJWT() user: Partial<User>, @UploadedFile(new ParseFilePipe({ validators: [new MaxFileSizeValidator({ maxSize: 1000000 }), new FileTypeValidator({ fileType: 'image/[jpeg,png]'})] })) file: Express.Multer.File, @Body() data: UpdateProfileDTO) {
+        let filename = '';
+
+        if (file) {
+            const [_, format] = file.mimetype.split('/');
+            filename = crypto.randomBytes(24).toString('hex')+'.'+format;
+
+            this.storageService.uploadFile(file.buffer, filename);
+        }
+
+        return this.profileService.saveProfile(user.id, data.bio, data.birthday, filename);
     }
 }
