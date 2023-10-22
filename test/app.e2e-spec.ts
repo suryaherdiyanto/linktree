a7a7ba3fd8fe3ed71e616a3d372cb13bd66a8a43
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-22 22:42:54 +0800

    fixed e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import * as session from 'express-session';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestExpressApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +15,21 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.setViewEngine('hbs');
+    app.use(session({
+      secret: 'secret-session-key',
+      saveUninitialized: false,
+      resave: false
+    }));
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(200);
   });
 });
