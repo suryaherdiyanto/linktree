ac967ba51d81f376bafb8a078a9ff0343d6552f4
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-22 20:59:39 +0800

    set view engine & session

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import session from 'express-session';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setViewEngine('hbs');
+
+  app.use(session({
+    secret: 'secret-session-key',
+    saveUninitialized: false,
+    resave: false
+  }));
   await app.listen(3000);
 }
 bootstrap();
