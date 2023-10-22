7e9357226830b9ada45e20a405cb941f12dd10e3
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-22 21:27:28 +0800

    set file storage for session & fixed index view

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,18 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as session from 'express-session';
+import * as FileStore from 'session-file-store';
 
+const fileStore = FileStore(session);
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', '..', 'views'));
+  app.useStaticAssets(join(__dirname, '..', '..', 'public'));
   app.setViewEngine('hbs');
 
   app.use(session({
+    store: new fileStore(),
     secret: 'secret-session-key',
     saveUninitialized: false,
     resave: false
