d1873b4963c606f1e77504d4f399a5554c6c3901
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-26 22:15:39 +0800

    reconfigure the socials.e2e-spec

--- test/socials.e2e-spec.ts
+++ test/socials.e2e-spec.ts
@@ -2,12 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import {users} from '../src/users/stubs/users.stub';
-import {getRepositoryToken} from '@nestjs/typeorm';
+import {TypeOrmModule, getRepositoryToken} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {User} from '../src/entities/users.entity';
 import * as Jwt from 'jsonwebtoken';
-import { AppModule } from '../src/app.module';
 import { Social, socials } from '../src/entities/socials.entity';
+import { ConfigModule } from '@nestjs/config';
+import { databaseOption } from 'src/config/database.config';
+import { ProfilesModule } from 'src/profiles/profiles.module';
 
 describe('SocialsController (e2e)', () => {
   let app: INestApplication;
@@ -20,7 +22,9 @@ describe('SocialsController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
-        AppModule
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRoot(databaseOption),
+        ProfilesModule
       ],
     }).compile();
 
