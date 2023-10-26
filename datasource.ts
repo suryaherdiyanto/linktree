5c64be8a26061abd2bc1e788c04c9bc2f618d28e
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-26 21:35:54 +0800

    update datasource using .env

--- datasource.ts
+++ datasource.ts
@@ -1,12 +1,19 @@
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+console.log(process.env);
 
 export default new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    username: "root",
-    password: "root",
-    database: "linktree",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: process.env.NODE_ENV === 'production' ? {
+        rejectUnauthorized: false
+    } : false,
     entities: ['./dist/src/entities/*.entity.js'],
     migrations: ['./dist/src/migrations/*.js'],
 });
