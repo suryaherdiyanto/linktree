d6b511355bc101b4777223d6e55f92554ca5c5ac
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-10-27 00:07:20 +0800

    remove foreign keys

--- src/migrations/1698323159177-migrations.ts
+++ src/migrations/1698323159177-migrations.ts
@@ -10,8 +10,6 @@ export class Migrations1698323159177 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`profile\` CHANGE \`birthday\` \`birthday\` date NULL`);
         await queryRunner.query(`ALTER TABLE \`profile\` CHANGE \`photo\` \`photo\` text NULL`);
         await queryRunner.query(`ALTER TABLE \`profile\` CHANGE \`userId\` \`userId\` varchar(36) NULL`);
-        await queryRunner.query(`ALTER TABLE \`social\` ADD CONSTRAINT \`FK_4cda297c26dea7a3b8d08b9ba18\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`profile\` ADD CONSTRAINT \`FK_a24972ebd73b106250713dcddd9\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -19,10 +17,8 @@ export class Migrations1698323159177 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`profile\` CHANGE \`photo\` \`photo\` text NULL DEFAULT 'NULL'`);
         await queryRunner.query(`ALTER TABLE \`profile\` CHANGE \`birthday\` \`birthday\` date NULL DEFAULT 'NULL'`);
         await queryRunner.query(`ALTER TABLE \`profile\` CHANGE \`bio\` \`bio\` varchar(200) NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`profile\` ADD CONSTRAINT \`FK_a24972ebd73b106250713dcddd9\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`user\` CHANGE \`access_token\` \`access_token\` text NULL DEFAULT 'NULL'`);
         await queryRunner.query(`ALTER TABLE \`social\` CHANGE \`userId\` \`userId\` varchar(36) NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`social\` ADD CONSTRAINT \`FK_4cda297c26dea7a3b8d08b9ba18\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
 }
